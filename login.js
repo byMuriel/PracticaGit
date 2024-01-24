93dfdf46c31bbd70cc38f9b8e51dd4661eb6bd77
MurielLogytel <mvitale@logytel.es>
2024-01-24 12:41:39 +0100

    Primer commit luego del merge  - main - login, octavo en total

--- login.js
+++ login.js
@@ -1,2 +1,3 @@
 console.log("Tercer commit - creacion del Login, actualizacion de esta linea");
 console.log("Quinto commit - nuevos datos en el login");
+console.log("Commit luego del merge main login");
--- main3.js
+++ main3.js
@@ -1,1 +1,2 @@
 console.log("Creacion de nuevo archivo main3.js");
+console.log("Commit luego del merge main login");
